4f28f2094a8b9b5001f53a8ae7901da423dd6a0b
tenisjavier <tenis.javier@gmail.com>
2025-11-20 15:12:30 -0300

    fix gestion siniestros

--- app/corredores-de-seguros/gestion-de-siniestros/page.tsx
+++ app/corredores-de-seguros/gestion-de-siniestros/page.tsx
@@ -3,13 +3,13 @@ import { fetchMetaTagsFromContentful, fetchPageComponents } from "@/config/db";
 import BuilderComponent from "@/components/BuilderComponent";
 
 export async function generateMetadata(): Promise<Metadata> {
-  const pathname = "/corredores-de-seguros/seguros-siniestros";
+  const pathname = "/corredores-de-seguros/gestion-de-siniestros";
   return await fetchMetaTagsFromContentful(pathname);
 }
 
 export default async function Home() {
   const components = await fetchPageComponents(
-    "/corredores-de-seguros/seguros-siniestros"
+    "/corredores-de-seguros/gestion-de-siniestros"
   );
   return <BuilderComponent components={components}></BuilderComponent>;
 }
