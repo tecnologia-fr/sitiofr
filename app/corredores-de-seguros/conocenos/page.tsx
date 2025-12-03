f3c711177ed1fd519936870d6d3c0bcee743d570
tenisjavier <tenis.javier@gmail.com>
2025-12-03 10:39:45 -0300

    fix

--- app/corredores-de-seguros/conocenos/page.tsx
+++ app/corredores-de-seguros/conocenos/page.tsx
@@ -3,13 +3,13 @@ import { fetchMetaTagsFromContentful, fetchPageComponents } from "@/config/db";
 import BuilderComponent from "@/components/BuilderComponent";
 
 export async function generateMetadata(): Promise<Metadata> {
-  const pathname = "/corredores-de-seguros/nosotros";
+  const pathname = "/corredores-de-seguros/conocenos";
   return await fetchMetaTagsFromContentful(pathname);
 }
 
 export default async function Home() {
   const components = await fetchPageComponents(
-    "/corredores-de-seguros/nosotros"
+    "/corredores-de-seguros/conocenos"
   );
   return <BuilderComponent components={components}></BuilderComponent>;
 }
