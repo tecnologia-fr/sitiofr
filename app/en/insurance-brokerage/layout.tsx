32b310f11f6c98d22121f834bcb1fd1b57909fe3
tenisjavier <tenis.javier@gmail.com>
2026-01-30 14:08:59 -0300

    footers

--- app/en/insurance-brokerage/layout.tsx
+++ app/en/insurance-brokerage/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Lato } from "next/font/google";
 import "../../globals.css";
 import { ResponsiveNavigationMenu } from "@/components/NavigationMenuCorredoresEN";
-import FooterCorredora from "@/components/Footer/footerCorredora";
+import FooterCorredoraEN from "@/components/Footer/footerCorredoraEN";
 import Image from "next/image";
 import Link from "next/link";
 import GTM from "@/config/tracking/gtm";
@@ -45,7 +45,7 @@ export default function InsuranceBrokerageLayout({
           </nav>
         </header>
         {children}
-        <FooterCorredora />
+        <FooterCorredoraEN />
       </body>
     </html>
   );
